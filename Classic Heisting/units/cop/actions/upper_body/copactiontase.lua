5e8e55e32453a7d9b86b968f9e8eb11ffc142698
gorgbus <63368174+gorgbus@users.noreply.github.com>
2021-08-24 12:49:32 +0200

    taser fix test

--- Classic Heisting/units/cop/actions/upper_body/copactiontase.lua
+++ Classic Heisting/units/cop/actions/upper_body/copactiontase.lua
@@ -1,5 +1,5 @@
---[[Hooks:PostHook(CopActionTase, "on_attention", "sh_on_attention", function (self)
+Hooks:PostHook(CopActionTase, "on_attention", "sh_on_attention", function (self)
     if self._line_of_fire_slotmask then
         self._line_of_fire_slotmask = self._line_of_fire_slotmask - managers.slot:get_mask("persons")
     end
-end)--]]
+end)

8dce1c1de571f2a37ac7c41778e8fffbaa39d274
gorgbus <63368174+gorgbus@users.noreply.github.com>
2021-08-24 23:47:25 +0200

    Update moneytweakdata.lua

--- Classic Heisting/tweak_data/moneytweakdata.lua
+++ Classic Heisting/tweak_data/moneytweakdata.lua
@@ -1,57 +1,57 @@
 Hooks:PostHook(MoneyTweakData, "init", "restore_init", function(self)
-	self.bag_values.default = 1000
-	self.bag_values.money = 1500
-	self.bag_values.gold = 2875
-	self.bag_values.diamonds = 1000
-	self.bag_values.coke = 2000
-	self.bag_values.meth = 13000
-	self.bag_values.meth_half = 6500
-	self.bag_values.weapon = 3000
-	self.bag_values.weapons = 3000
-	self.bag_values.painting = 3000
-	self.bag_values.samurai_suit = 5000
-	self.bag_values.artifact_statue = 5000
-	self.bag_values.mus_artifact_bag = 1000
+	self.bag_values.default = 900
+	self.bag_values.money = 750
+	self.bag_values.gold = 1000
+	self.bag_values.diamonds = 500
+	self.bag_values.coke = 900
+	self.bag_values.meth = 1000
+	self.bag_values.meth_half = 500
+	self.bag_values.weapon = 950
+	self.bag_values.weapons = 950
+	self.bag_values.painting = 700
+	self.bag_values.samurai_suit = 2000
+	self.bag_values.artifact_statue = 2000
+	self.bag_values.mus_artifact_bag = 700
 	self.bag_values.circuit = 1000
-	self.bag_values.sandwich = 10000
-	self.bag_values.cro_loot = 10000
-	self.bag_values.hope_diamond = 30000
-	self.bag_values.evidence_bag = 3000
-	self.bag_values.vehicle_falcogini = 4000
-	self.bag_values.warhead = 4600
-	self.bag_values.unknown = 5000
-	self.bag_values.safe = 4600
+	self.bag_values.sandwich = 3000
+	self.bag_values.cro_loot = 5000
+	self.bag_values.hope_diamond = 10000
+	self.bag_values.evidence_bag = 1500
+	self.bag_values.vehicle_falcogini = 2000
+	self.bag_values.warhead = 2300
+	self.bag_values.unknown = 2500
+	self.bag_values.safe = 2300
 	self.bag_values.prototype = 5000
-	self.bag_values.faberge_egg = 3000
-	self.bag_values.treasure = 3200
-	self.bag_values.counterfeit_money = 1100
+	self.bag_values.faberge_egg = 1500
+	self.bag_values.treasure = 1600
+	self.bag_values.counterfeit_money = 600
 	self.bag_values.box_unknown = 5000
 	self.bag_values.black_tablet = 5000
-	self.bag_values.masterpiece_painting = 10000
-	self.bag_values.master_server = 10000
-	self.bag_values.lost_artifact = 10000
-	self.bag_values.present = 2049
-	self.bag_values.mad_master_server_value_1 = 5000
-	self.bag_values.mad_master_server_value_2 = 10000
-	self.bag_values.mad_master_server_value_3 = 15000
-	self.bag_values.mad_master_server_value_4 = 20000
-	self.bag_values.weapon_glock = 2000
-	self.bag_values.weapon_scar = 2000
-	self.bag_values.drk_bomb_part = 9000
+	self.bag_values.masterpiece_painting = 5000
+	self.bag_values.master_server = 5000
+	self.bag_values.lost_artifact = 5000
+	self.bag_values.present = 1000
+	self.bag_values.mad_master_server_value_1 = 2500
+	self.bag_values.mad_master_server_value_2 = 5000
+	self.bag_values.mad_master_server_value_3 = 7500
+	self.bag_values.mad_master_server_value_4 = 10000
+	self.bag_values.weapon_glock = 1000
+	self.bag_values.weapon_scar = 1000
+	self.bag_values.drk_bomb_part = 4500
 	self.bag_values.drone_control_helmet = 9000
 	self.bag_values.toothbrush = 9000
-	self.bag_values.cloaker_gold = 3000
-	self.bag_values.cloaker_money  = 2250
-	self.bag_values.cloaker_cocaine = 2700
+	self.bag_values.cloaker_gold = 1000
+	self.bag_values.cloaker_money  = 750
+	self.bag_values.cloaker_cocaine = 900
 	self.bag_values.diamond_necklace = 1500
-	self.bag_values.vr_headset = 3000
-	self.bag_values.women_shoes = 3000
-	self.bag_values.expensive_vine = 3000
-	self.bag_values.ordinary_wine = 3000
-	self.bag_values.robot_toy = 3000
-	self.bag_values.rubies = 1500
-	self.bag_values.red_diamond = 10000
-	self.bag_values.old_wine = 2000
+	self.bag_values.vr_headset = 1500
+	self.bag_values.women_shoes = 1500
+	self.bag_values.expensive_vine = 1500
+	self.bag_values.ordinary_wine = 1500
+	self.bag_values.robot_toy = 1500
+	self.bag_values.rubies = 750
+	self.bag_values.red_diamond = 5000
+	self.bag_values.old_wine = 1000
 	self.bag_values.shells = 2100
 	self.bag_values.turret = 10000
 	self.buy_premium_multiplier = {
@@ -73,15 +73,6 @@ Hooks:PostHook(MoneyTweakData, "init", "restore_init", function(self)
 		easy_wish = 300000,
 		overkill_290 = 360000,
 		sm_wish = 360000
-	}
-	self.difficulty_multiplier = {
-		4,
-		9,
-		12,
-		20,
-		40,
-		0,
-		0
 	}
 	local smallest_cashout = (self.stage_completion[1] + self.job_completion[1]) * self.offshore_rate
 	local biggest_mask_cost = self.biggest_cashout * 40

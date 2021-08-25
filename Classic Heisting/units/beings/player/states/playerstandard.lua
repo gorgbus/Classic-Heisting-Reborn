324254b915c61ed75c70a4f7729c4523b4669cf1
gorgbus <63368174+gorgbus@users.noreply.github.com>
2021-08-25 10:55:21 +0200

    fixed inspire

--- Classic Heisting/units/beings/player/states/playerstandard.lua
+++ Classic Heisting/units/beings/player/states/playerstandard.lua
@@ -50,3 +50,172 @@ function PlayerStandard:_get_max_walk_speed(t, force_run)
 
 	return final_speed
 end
+
+function PlayerStandard:_get_intimidation_action(prime_target, char_table, amount, primary_only, detect_only, secondary)
+	local voice_type, new_action, plural = nil
+	local unit_type_enemy = 0
+	local unit_type_civilian = 1
+	local unit_type_teammate = 2
+	local unit_type_camera = 3
+	local unit_type_turret = 4
+	local is_whisper_mode = managers.groupai:state():whisper_mode()
+
+	if prime_target then
+		if prime_target.unit_type == unit_type_teammate then
+			local is_human_player, record = nil
+
+			if not detect_only then
+				record = managers.groupai:state():all_criminals()[prime_target.unit:key()]
+
+				if record.ai then
+					if not prime_target.unit:brain():player_ignore() then
+						prime_target.unit:movement():set_cool(false)
+						prime_target.unit:brain():on_long_dis_interacted(0, self._unit, secondary)
+					end
+				else
+					is_human_player = true
+				end
+			end
+
+			local amount = 0
+			local current_state_name = self._unit:movement():current_state_name()
+
+			if current_state_name ~= "arrested" and current_state_name ~= "bleed_out" and current_state_name ~= "fatal" and current_state_name ~= "incapacitated" then
+				local rally_skill_data = self._ext_movement:rally_skill_data()
+
+				if rally_skill_data and mvector3.distance_sq(self._pos, record.m_pos) < rally_skill_data.range_sq then
+					local needs_revive, is_arrested, action_stop = nil
+
+					if not secondary then
+						if prime_target.unit:base().is_husk_player then
+							is_arrested = prime_target.unit:movement():current_state_name() == "arrested"
+							needs_revive = prime_target.unit:interaction():active() and prime_target.unit:movement():need_revive() and not is_arrested
+						else
+							is_arrested = prime_target.unit:character_damage():arrested()
+							needs_revive = prime_target.unit:character_damage():need_revive()
+						end
+
+						if needs_revive and rally_skill_data.long_dis_revive then
+							voice_type = "revive"
+						elseif not is_arrested and not needs_revive and rally_skill_data.morale_boost_delay_t and rally_skill_data.morale_boost_delay_t < managers.player:player_timer():time() then
+							voice_type = "boost"
+							amount = 1
+						end
+					end
+				end
+			end
+
+			if is_human_player then
+				prime_target.unit:network():send_to_unit({
+					"long_dis_interaction",
+					prime_target.unit,
+					amount,
+					self._unit,
+					secondary or false
+				})
+			end
+
+			voice_type = voice_type or secondary and "ai_stay" or "come"
+			plural = false
+		else
+			local prime_target_key = prime_target.unit:key()
+
+			if prime_target.unit_type == unit_type_enemy then
+				plural = false
+
+				if prime_target.unit:anim_data().hands_back then
+					voice_type = "cuff_cop"
+				elseif prime_target.unit:anim_data().surrender then
+					voice_type = "down_cop"
+				elseif is_whisper_mode and prime_target.unit:movement():cool() and prime_target.unit:base():char_tweak().silent_priority_shout then
+					voice_type = "mark_cop_quiet"
+				elseif prime_target.unit:base():char_tweak().priority_shout then
+					voice_type = "mark_cop"
+				else
+					voice_type = "stop_cop"
+				end
+			elseif prime_target.unit_type == unit_type_camera then
+				if not prime_target.unit or not prime_target.unit:base() or not prime_target.unit:base().is_friendly then
+					plural = false
+					voice_type = "mark_camera"
+				end
+			elseif prime_target.unit_type == unit_type_turret then
+				plural = false
+				voice_type = "mark_turret"
+			elseif prime_target.unit:base():char_tweak().is_escort then
+				plural = false
+				local e_guy = prime_target.unit
+
+				if e_guy:anim_data().move then
+					voice_type = "escort_keep"
+				elseif e_guy:anim_data().panic then
+					voice_type = "escort_go"
+				else
+					voice_type = prime_target.unit:base():char_tweak().speech_escort or "escort"
+				end
+			else
+				if prime_target.unit:movement():stance_name() == "cbt" and prime_target.unit:anim_data().stand then
+					voice_type = "come"
+				elseif prime_target.unit:anim_data().move then
+					voice_type = "stop"
+				elseif prime_target.unit:anim_data().drop then
+					voice_type = "down_stay"
+				else
+					voice_type = "down"
+				end
+
+				local num_affected = 0
+
+				for _, char in pairs(char_table) do
+					if char.unit_type == unit_type_civilian then
+						if voice_type == "stop" and char.unit:anim_data().move then
+							num_affected = num_affected + 1
+						elseif voice_type == "down_stay" and char.unit:anim_data().drop then
+							num_affected = num_affected + 1
+						elseif voice_type == "down" and not char.unit:anim_data().move and not char.unit:anim_data().drop then
+							num_affected = num_affected + 1
+						end
+					end
+				end
+
+				if num_affected > 1 then
+					plural = true
+				else
+					plural = false
+				end
+			end
+
+			local max_inv_wgt = 0
+
+			for _, char in pairs(char_table) do
+				if max_inv_wgt < char.inv_wgt then
+					max_inv_wgt = char.inv_wgt
+				end
+			end
+
+			if max_inv_wgt < 1 then
+				max_inv_wgt = 1
+			end
+
+			if detect_only then
+				voice_type = "come"
+			else
+				for _, char in pairs(char_table) do
+					if char.unit_type ~= unit_type_camera and char.unit_type ~= unit_type_teammate and (not is_whisper_mode or not char.unit:movement():cool()) then
+						if char.unit_type == unit_type_civilian then
+							amount = (amount or tweak_data.player.long_dis_interaction.intimidate_strength) * managers.player:upgrade_value("player", "civ_intimidation_mul", 1) * managers.player:team_upgrade_value("player", "civ_intimidation_mul", 1)
+						end
+
+						if prime_target_key == char.unit:key() then
+							voice_type = char.unit:brain():on_intimidated(amount or tweak_data.player.long_dis_interaction.intimidate_strength, self._unit) or voice_type
+						elseif not primary_only and char.unit_type ~= unit_type_enemy then
+							char.unit:brain():on_intimidated((amount or tweak_data.player.long_dis_interaction.intimidate_strength) * char.inv_wgt / max_inv_wgt, self._unit)
+						end
+					end
+				end
+			end
+		end
+	end
+
+	return voice_type, plural, prime_target
+end

f7b557b66d71ebd871127154c541884cdee011de
gorgbus <63368174+gorgbus@users.noreply.github.com>
2021-08-26 23:17:58 +0200

    hidden HEF granade

--- Classic Heisting/tweak_data/projectilestweakdata.lua
+++ Classic Heisting/tweak_data/projectilestweakdata.lua
@@ -18,22 +18,6 @@ function BlackMarketTweakData:_init_projectiles(tweak_data)
 	self.projectiles.frag.is_a_grenade = true
 	self.projectiles.frag.is_explosive = true
 	self.projectiles.frag_com = {
-		name_id = "bm_grenade_frag_com",
-		desc_id = "bm_grenade_frag_com_desc",
-		unit = "units/payday2/weapons/wpn_frag_grenade_com/wpn_frag_grenade_com",
-		unit_dummy = "units/payday2/weapons/wpn_frag_grenade_com/wpn_frag_grenade_com_husk",
-		sprint_unit = "units/payday2/weapons/wpn_frag_grenade_com/wpn_frag_grenade_com_sprint",
-		icon = "frag_grenade",
-		throwable = true,
-		max_amount = 3,
-		animation = "throw_grenade_com",
-		anim_global_param = "projectile_frag_com",
-		throw_allowed_expire_t = 0.1,
-		expire_t = 1.1,
-		repeat_expire_t = 1.5,
-		is_a_grenade = true,
-		is_explosive = true,
-		dlc = "pd2_clan"
 	}
 	self._projectiles_index = {
 		"frag"

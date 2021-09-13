b3c9e6824762c7086d394623f901d1f580060524
gorgbus <63368174+gorgbus@users.noreply.github.com>
2021-09-13 22:58:45 +0200

    removal of useless code

--- Classic Heisting/tweak_data/blackmarket/playerstyletweakdata.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(BlackMarketTweakData, "_init_player_styles", "hide_outfits", function(self)
-    self.player_styles.general = nil
-    self.player_styles.gangstercoat = nil
-    self.player_styles.leather = nil
-    self.player_styles.t800 = nil
-    self.player_styles.baron = nil
-end)

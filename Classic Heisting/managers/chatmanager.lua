08e5ad3d46c2c6fa3862034c30699d1d94c38ade
MiamiCenterPL <dawidow735@gmail.com>
2021-10-09 19:36:52 +0200

    Fix console error.

--- Classic Heisting/managers/chatmanager.lua
+++ Classic Heisting/managers/chatmanager.lua
@@ -1,10 +1,12 @@
 ChatBase = ChatBase or class()
 ChatGui = ChatGui or class(ChatBase)
 
-ChatGui.PRESETS.inventory.chat_button_x_offset = 50
-ChatGui.PRESETS.inventory.chat_button_y_offset = 21
-ChatGui.PRESETS.lobby.chat_button_y_offset = 60
-ChatGui.PRESETS.default.chat_button_y_offset = 50
+if ChatGui.PRESETS then --Fix errors in console.
+	ChatGui.PRESETS.inventory.chat_button_x_offset = 50
+	ChatGui.PRESETS.inventory.chat_button_y_offset = 21
+	ChatGui.PRESETS.lobby.chat_button_y_offset = 60
+	ChatGui.PRESETS.default.chat_button_y_offset = 50
+end
 
 Hooks:PreHook(PlayerInventoryGui, "_update_player_stats", "PlayerInventoryGui_update_player_stats", function(self)
 	local player_panel = self._panel:child("player_panel")

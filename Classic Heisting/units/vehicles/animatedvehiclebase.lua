a635cf8859a7d8a23217c4f33d5b521f51f18a31
gorgbus <63368174+gorgbus@users.noreply.github.com>
2021-10-13 19:31:03 +0200

    fixed Black Cat turret

--- Classic Heisting/units/vehicles/animatedvehiclebase.lua
+++ Classic Heisting/units/vehicles/animatedvehiclebase.lua
@@ -1,6 +1,6 @@
 set_animated_vehicle_base_spawn_original = AnimatedVehicleBase.spawn_module
 local level = Global.level_data and Global.level_data.level_id or ''
-if level ~= "des" then
+if level ~= "des" and level ~= "chca" then
 	function AnimatedVehicleBase:spawn_module(module_unit_name, ...)
 		local level = Global.level_data and Global.level_data.level_id or ''
 		if type_name(module_unit_name) == "spawn_turret" then

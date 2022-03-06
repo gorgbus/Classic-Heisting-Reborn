22f811203a464c23a6eb6c9f2d9a70cc386e9403
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-03-06 20:13:00 +0100

    hopefully fixed crash

--- Classic Heisting/units/cop/copdamage.lua
+++ Classic Heisting/units/cop/copdamage.lua
@@ -1,5 +1,7 @@
-Hooks:PostHook(CopDamage, "die", "messiah_pistol_rev", function(self, attack_data)
-	if attack_data.attacker_unit:character_damage():bleed_out() and not self:is_civilian(self._unit:base()._tweak_table) then
+Hooks:PreHook(CopDamage, "die", "messiah_pistol_rev", function(self, attack_data)
+    local bleed_out = attack_data.attacker_unit:character_damage().bleed_out and attack_data.attacker_unit:character_damage():bleed_out() or false
+
+	if bleed_out and not self:is_civilian(self._unit:base()._tweak_table) then
         local messiah_revive = false
         if managers.player:has_category_upgrade("player", "pistol_revive_from_bleed_out") and attack_data.weapon_unit:base():weapon_tweak_data().category == "pistol" and attack_data.attacker_unit:character_damage():consume_messiah_charge() then
             messiah_revive = true

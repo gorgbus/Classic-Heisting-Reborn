2ae0518f5f9455ce6369ed2ec0d069a9f6cab3ec
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-03-26 00:26:23 +0100

    fixing more copdamage crashes

--- Classic Heisting/units/cop/copdamage.lua
+++ Classic Heisting/units/cop/copdamage.lua
@@ -1,5 +1,5 @@
 Hooks:PreHook(CopDamage, "die", "messiah_pistol_rev", function(self, attack_data)
-    local bleed_out = attack_data.attacker_unit:character_damage().bleed_out and attack_data.attacker_unit:character_damage():bleed_out() or false
+    local bleed_out = attack_data.attacker_unit and (attack_data.attacker_unit:character_damage().bleed_out and attack_data.attacker_unit:character_damage():bleed_out() or false) or false
 
 	if bleed_out and not self:is_civilian(self._unit:base()._tweak_table) then
         local messiah_revive = false

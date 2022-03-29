749b0390a6faeb8d023b834b7c7ae49ba8e86bc4
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-03-29 20:48:04 +0200

    fixed weapon spread

--- Classic Heisting/units/weapons/raycastweaponbase.lua
+++ Classic Heisting/units/weapons/raycastweaponbase.lua
@@ -1,97 +1,63 @@
-function RaycastWeaponBase:fire(from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul, target_unit)
-	if managers.player:has_activate_temporary_upgrade("temporary", "no_ammo_cost_buff") then
-		managers.player:deactivate_temporary_upgrade("temporary", "no_ammo_cost_buff")
-
-		if managers.player:has_category_upgrade("temporary", "no_ammo_cost") then
-			managers.player:activate_temporary_upgrade("temporary", "no_ammo_cost")
-		end
+local mvec_to = Vector3()
+local mvec_spread_direction = Vector3()
+function RaycastWeaponBase:_fire_raycast(user_unit, from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul)
+	local result = {}
+	local hit_unit
+	local spread = self:_get_spread(user_unit)
+	mvector3.set(mvec_spread_direction, direction)
+	if spread then
+		mvector3.spread(mvec_spread_direction, spread * (spread_mul or 1))
 	end
-
-	if self._bullets_fired then
-		if self._bullets_fired == 1 and self:weapon_tweak_data().sounds.fire_single then
-			self:play_tweak_data_sound("stop_fire")
-			self:play_tweak_data_sound("fire_auto", "fire")
-		end
-		self._bullets_fired = self._bullets_fired + 1
-	end
-
-	local is_player = self._setup.user_unit == managers.player:player_unit()
-	local consume_ammo = not managers.player:has_activate_temporary_upgrade("temporary", "no_ammo_cost") and (not managers.player:has_activate_temporary_upgrade("temporary", "berserker_damage_multiplier") or not managers.player:has_category_upgrade("player", "berserker_no_ammo_cost")) or not is_player
-
-	if consume_ammo and (is_player or Network:is_server()) then
-		local base = self:ammo_base()
-
-		if base:get_ammo_remaining_in_clip() == 0 then
-			return
-		end
-
-		local ammo_usage = 1
-
-		if is_player then
-			for _, category in ipairs(self:weapon_tweak_data().categories) do
-				if managers.player:has_category_upgrade(category, "consume_no_ammo_chance") then
-					local roll = math.rand(1)
-					local chance = managers.player:upgrade_value(category, "consume_no_ammo_chance", 0)
-
-					if roll < chance then
-						ammo_usage = 0
-
-						print("NO AMMO COST")
-					end
-				end
+	mvector3.set(mvec_to, mvec_spread_direction)
+	mvector3.multiply(mvec_to, 20000)
+	mvector3.add(mvec_to, from_pos)
+	local damage = self:_get_current_damage(dmg_mul)
+	local col_ray = World:raycast("ray", from_pos, mvec_to, "slot_mask", self._bullet_slotmask, "ignore_unit", self._setup.ignore_units)
+	local autoaim, suppression_enemies = self:check_autoaim(from_pos, direction)
+	if self._autoaim then
+		local weight = 0.1
+		if col_ray and col_ray.unit:in_slot(managers.slot:get_mask("enemies")) then
+			self._autohit_current = (self._autohit_current + weight) / (1 + weight)
+			hit_unit = InstantBulletBase:on_collision(col_ray, self._unit, user_unit, damage)
+		elseif autoaim then
+			local autohit_chance = 1 - math.clamp((self._autohit_current - self._autohit_data.MIN_RATIO) / (self._autohit_data.MAX_RATIO - self._autohit_data.MIN_RATIO), 0, 1)
+			if autohit_mul then
+				autohit_chance = autohit_chance * autohit_mul
 			end
+			if autohit_chance > math.random() then
+				self._autohit_current = (self._autohit_current + weight) / (1 + weight)
+				hit_unit = InstantBulletBase:on_collision(autoaim, self._unit, user_unit, damage)
+			else
+				self._autohit_current = self._autohit_current / (1 + weight)
+			end
+		elseif col_ray then
+			hit_unit = InstantBulletBase:on_collision(col_ray, self._unit, user_unit, damage)
 		end
-
-		local mag = base:get_ammo_remaining_in_clip()
-		local remaining_ammo = mag - ammo_usage
-
-		if mag > 0 and remaining_ammo <= (self.AKIMBO and 1 or 0) then
-			local w_td = self:weapon_tweak_data()
-
-			if w_td.animations and w_td.animations.magazine_empty then
-				self:tweak_data_anim_play("magazine_empty")
-			end
-
-			if w_td.sounds and w_td.sounds.magazine_empty then
-				self:play_tweak_data_sound("magazine_empty")
-			end
-
-			if w_td.effects and w_td.effects.magazine_empty then
-				self:_spawn_tweak_data_effect("magazine_empty")
-			end
-
-			self:set_magazine_empty(true)
-		end
-
-		base:set_ammo_remaining_in_clip(base:get_ammo_remaining_in_clip() - ammo_usage)
-		self:use_ammo(base, ammo_usage)
+		self._shot_fired_stats_table.hit = hit_unit and true or false
+		managers.statistics:shot_fired(self._shot_fired_stats_table)
+	elseif col_ray then
+		hit_unit = InstantBulletBase:on_collision(col_ray, self._unit, user_unit, damage)
 	end
-
-	local user_unit = self._setup.user_unit
-
-	self:_check_ammo_total(user_unit)
-
-	if alive(self._obj_fire) then
-		self:_spawn_muzzle_effect(from_pos, direction)
+	if suppression_enemies and self._suppression then
+		result.enemies_in_cone = suppression_enemies
 	end
-
-	self:_spawn_shell_eject_effect()
-
-	local ray_res = self:_fire_raycast(user_unit, from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul, target_unit)
-
-	if self._alert_events and ray_res.rays then
-		self:_check_alert(ray_res.rays, from_pos, direction, user_unit)
-	end
-
-	if ray_res.enemies_in_cone then
-		for enemy_data, dis_error in pairs(ray_res.enemies_in_cone) do
-			if not enemy_data.unit:movement():cool() then
-				enemy_data.unit:character_damage():build_suppression(suppr_mul * dis_error * self._suppression, self._panic_suppression_chance)
-			end
+	if hit_unit and hit_unit.type == "death" and self:weapon_tweak_data().category == tweak_data.achievement.easy_as_breathing.weapon_type then
+		self._kills_without_releasing_trigger = (self._kills_without_releasing_trigger or 0) + 1
+		if self._kills_without_releasing_trigger >= tweak_data.achievement.easy_as_breathing.count then
+			managers.achievment:award(tweak_data.achievement.easy_as_breathing.award)
 		end
 	end
-
-	managers.player:send_message(Message.OnWeaponFired, nil, self._unit, ray_res)
-
-	return ray_res
+	if (col_ray and col_ray.distance > 600 or not col_ray) and alive(self._obj_fire) then
+		self._obj_fire:m_position(self._trail_effect_table.position)
+		mvector3.set(self._trail_effect_table.normal, mvec_spread_direction)
+		local trail = World:effect_manager():spawn(self._trail_effect_table)
+		if col_ray then
+			World:effect_manager():set_remaining_lifetime(trail, math.clamp((col_ray.distance - 600) / 10000, 0, col_ray.distance))
+		end
+	end
+	result.hit_enemy = hit_unit
+	if self._alert_events then
+		result.rays = {col_ray}
+	end
+	return result
 end
